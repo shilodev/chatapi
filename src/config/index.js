8e195200cf5a02df0f08665c175456f9dfd6f586
RAYEN905 <mr.rayenk@gmail.com>
2020-09-18 15:06:12 -0400

    mongo

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,6 @@ export default {
   // "port": 3005,
   // "mongoUrl": "mongodb://localhost:27017/chat-api",
   "port": process.env.PORT,
-  "mongoUrl": process.env.MONGODB_URI,
+  "mongoUrl": "mongodb://hamboy:Skamta12!@ds015879.mlab.com:15879/iheard"
   "bodyLimit": "100kb"
 }
